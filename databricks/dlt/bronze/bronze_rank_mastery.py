621e32aa02bd4812f24dba543246d3f131887cf3
douglasmnegri <douglasmnegri@gmail.com>
2026-02-28 23:26:48 +0000

    feat: add new file to the bronze layer that reads the rank from azure blob storage

---
+++ databricks/dlt/bronze/bronze_rank_mastery.py
@@ -0,0 +1,13 @@
+from pyspark.sql.functions import current_timestamp
+
+storage_account = "striotdatapipeline001"
+
+RAW_PATH = f"abfss://riot-data@{storage_account}.dfs.core.windows.net/data/riot/lol/rank"
+
+df = (
+    spark.read
+        .option("multiLine", "true")
+        .json(RAW_PATH)
+)
+
+display(df)
