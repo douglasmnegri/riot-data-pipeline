e281e3cc129988a971399c72285d63167eb130c1
douglasmnegri <douglasmnegri@gmail.com>
2026-02-28 23:01:55 +0000

    feat: update azure blob storage access path

--- databricks/dlt/bronze.py
+++ databricks/dlt/bronze.py
@@ -1,22 +1,13 @@
-import dlt
 from pyspark.sql.functions import current_timestamp
 
-spark.conf.set(
-    "fs.azure.account.key.riotdata.blob.core.windows.net",
-    dbutils.secrets.get("riot-scope", "riotdata-access-key")
+storage_account = "striotdatapipeline001"
+
+RAW_PATH = f"abfss://riot-data@{storage_account}.dfs.core.windows.net/data/riot/lol/champion_mastery"
+
+df = (
+    spark.read
+        .option("multiLine", "true")
+        .json(RAW_PATH)
 )
 
-RAW_PATH = "wasbs://riot-data-pipeline@riotdata.blob.core.windows.net/puuids/champion_mastery"
-
-@dlt.table(
-    name="matches_bronze",
-    comment="Raw champion mastery JSON ingested from Riot API",
-    table_properties={"quality": "bronze"}
-)
-def matches_bronze():
-    return (
-        spark.read
-            .option("multiLine", "true")
-            .json(RAW_PATH)
-            .withColumn("_ingestion_ts", current_timestamp())
-    )
+display(df)
