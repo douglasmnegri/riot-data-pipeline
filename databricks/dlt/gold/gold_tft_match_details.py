13b1d7045ed2e7d377a1a6d2c330102d4c2b2bc2
douglasmnegri <douglasmnegri@gmail.com>
2026-03-02 21:34:35 +0000

    feat: add tft match_details to the gold layer

---
+++ databricks/dlt/gold/gold_tft_match_details.py
@@ -0,0 +1,34 @@
+storage_account = "striotdatapipeline001"
+
+GOLD_BASE_PATH = f"abfss://riot-data@{storage_account}.dfs.core.windows.net/delta/gold"
+
+GOLD_PLAYER_STATS_PATH = f"{GOLD_BASE_PATH}/tft_player_stats"
+GOLD_TRAIT_STATS_PATH = f"{GOLD_BASE_PATH}/tft_trait_stats"
+GOLD_UNIT_STATS_PATH = f"{GOLD_BASE_PATH}/tft_unit_stats"
+
+
+df_gold_most_played_traits = spark.sql("""
+SELECT
+    name as trait_name,
+    COUNT(name) AS number_of_trait_appearence
+FROM silver.tft_traits
+GROUP BY name
+ORDER BY number_of_trait_appearence DESC
+""")
+
+
+
+
+df_gold_traits_placement = spark.sql("""
+SELECT
+    t.name AS trait_name,
+    t.num_units,
+    COUNT(*) AS appearances,
+    AVG(p.placement) AS avg_placement
+FROM silver.tft_traits t
+JOIN silver.tft_participants p
+    ON t.gameId = p.gameId
+    AND t.puuid = p.puuid
+GROUP BY t.name, t.num_units
+ORDER BY t.name, t.num_units
+""").display()
