2e34d4b746d55b2e063cac55f11f40033821245f
douglasmnegri <douglasmnegri@gmail.com>
2026-03-02 19:48:24 +0000

    feat: add new file to bronze layer that reads data from tft player details

---
+++ databricks/dlt/bronze/bronze_tft_player_details.py
@@ -0,0 +1,14 @@
+from pyspark.sql.functions import current_timestamp
+
+storage_account = "striotdatapipeline001"
+
+RAW_PATH = f"abfss://riot-data@{storage_account}.dfs.core.windows.net/data/riot/tft/player_details"
+
+df = (
+    spark.read
+        .option("multiLine", "true")
+        .json(RAW_PATH)
+)
+
+
+df.display()
