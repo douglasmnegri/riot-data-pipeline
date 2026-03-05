b7930448753737eb93e90c865bfb5032e7e34d6e
douglasmnegri <douglasmnegri@gmail.com>
2026-03-05 18:28:04 +0000

    feat: add new raw ingestion to bronze layer of player_details

--- databricks/dlt/bronze/bronze_tft_player_details.py
+++ databricks/dlt/bronze/bronze_tft_player_details.py
@@ -1,14 +1,37 @@
-from pyspark.sql.functions import current_timestamp
+from pyspark.sql.functions import current_timestamp, input_file_name
 
 storage_account = "striotdatapipeline001"
 
 RAW_PATH = f"abfss://riot-data@{storage_account}.dfs.core.windows.net/data/riot/tft/player_details"
+BRONZE_PATH = f"abfss://riot-data@{storage_account}.dfs.core.windows.net/delta/bronze/tft_player_details"
 
-df = (
+spark.sql(f"""
+CREATE SCHEMA IF NOT EXISTS bronze
+MANAGED LOCATION 'abfss://riot-data@{storage_account}.dfs.core.windows.net/delta/bronze'
+""")
+
+df_raw = (
     spark.read
         .option("multiLine", "true")
         .json(RAW_PATH)
 )
 
+df_bronze = (
+    df_raw
+        .withColumn("ingestion_timestamp", current_timestamp())
+        .withColumn("source_file", input_file_name())
+)
 
-df.display()
+
+df_bronze.write \
+    .format("delta") \
+    .mode("append") \
+    .save(BRONZE_PATH)
+
+spark.sql(f"""
+CREATE TABLE IF NOT EXISTS bronze.tft_player_details
+USING DELTA
+LOCATION '{BRONZE_PATH}'
+""")
+
+display(spark.read.table("bronze.tft_player_details"))
